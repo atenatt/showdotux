11e7e883e1c8723b4824b2b3deccf49455fc04e7
atenatt <victor.boas@fatec.sp.gov.br>
2024-11-14 11:40:17 -0300

    style: remove box categorias select

--- app/configs/04-jogo.sh
+++ app/configs/04-jogo.sh
@@ -5,9 +5,6 @@ function iniciar_jogo() {
     local categorias
     categorias=$(selecionar_categorias)
 
-    # Confirma as categorias selecionadas ao jogador
-    dialog --msgbox "Categorias selecionadas: $categorias" 6 50
-
     # Formata as categorias para serem usadas na query SQL
     local categorias_formatadas
     categorias_formatadas=$(echo "$categorias" | sed "s/ /','/g" | sed "s/^/'/;s/$/'/")
@@ -19,6 +16,7 @@ function iniciar_jogo() {
          FROM perguntas 
          WHERE categoria IN ($categorias_formatadas) 
          ORDER BY random() 
+         
          LIMIT 10;")
 
     # Verifica se as perguntas foram obtidas
@@ -32,30 +30,28 @@ function iniciar_jogo() {
 
     # Loop de perguntas
     while IFS='|' read -r enunciado opcao_a opcao_b opcao_c opcao_d resposta_correta; do
-        if [[ -z "$enunciado" || -z "$opcao_a" || -z "$opcao_b" || -z "$opcao_c" || -z "$opcao_d" || -z "$resposta_correta" ]]; then
-            dialog --msgbox "Erro ao carregar pergunta. Verifique o banco de dados." 6 50
-            return
-        fi
-
+        # Remove espaços extras e formatação invisível das respostas
+        resposta_correta=$(echo "$resposta_correta" | xargs)
+        
         resposta=$(dialog --menu "$enunciado" 15 50 6 \
             A "$opcao_a" \
             B "$opcao_b" \
             C "$opcao_c" \
             D "$opcao_d" \
             P "Pular (Restantes: $pulos_restantes)" 3>&1 1>&2 2>&3)
-
+        
         if [[ $resposta == "P" && $pulos_restantes -gt 0 ]]; then
             ((pulos_restantes--))
             continue
         elif [[ $resposta == "$resposta_correta" ]]; then
             ((pontuacao+=10))
-            dialog --msgbox "Correto! Pontuação atual: $pontuacao" 6 50
+            dialog --msgbox "Correto! Pontuacao atual: $pontuacao" 6 50
         else
             dialog --msgbox "Errado! A resposta correta era $resposta_correta" 6 50
         fi
     done <<< "$perguntas"
 
-    dialog --msgbox "Jogo finalizado! Sua pontuação: $pontuacao" 6 50
+    dialog --msgbox "Jogo finalizado! Sua pontuacao: $pontuacao" 6 50
     salvar_pontuacao $pontuacao
 }
 
