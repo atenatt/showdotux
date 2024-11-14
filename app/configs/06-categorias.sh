7b12b60048c4d5576106359858556e03556d4b4c
atenatt <victor.boas@fatec.sp.gov.br>
2024-11-14 11:03:19 -0300

    feat: refactory and remove text file using

--- app/configs/06-categorias.sh
+++ app/configs/06-categorias.sh
@@ -1,29 +1,25 @@
 #!/bin/bash
 
 function selecionar_categorias() {
+    local categorias_disponiveis
     categorias_disponiveis=$(psql -h $DB_HOST -U $DB_USER -d $DB_NAME -t -c \
-        "SELECT id, nome FROM categorias;" | xargs -L1)
+        "SELECT nome FROM categorias;" | xargs -L1)
 
-    # Converte as categorias em uma lista para o dialog
-    categorias_menu=()
-    while IFS= read -r linha; do
-        id=$(echo "$linha" | awk '{print $1}')
-        nome=$(echo "$linha" | awk '{print $2}')
-        categorias_menu+=("$id" "$nome" "off")
+    local categorias_menu=()
+    while IFS= read -r categoria; do
+        categorias_menu+=("$categoria" "$categoria" "off")
     done <<< "$categorias_disponiveis"
 
     while true; do
-        dialog --checklist "Selecione pelo menos uma categoria:" 15 50 5 \
-            "${categorias_menu[@]}" 2>categorias_escolhidas.txt
+        local selecionadas
+        selecionadas=$(dialog --checklist "Selecione pelo menos uma categoria:" 15 50 5 \
+            "${categorias_menu[@]}" 3>&1 1>&2 2>&3)
 
-        categorias=$(cat categorias_escolhidas.txt | tr -d '"')
-
-        if [[ -z "$categorias" ]]; then
-            dialog --msgbox "Voce deve selecionar pelo menos uma categoria!" 6 50
+        if [[ -z "$selecionadas" ]]; then
+            dialog --msgbox "Você deve selecionar pelo menos uma categoria!" 6 50
         else
+            echo "$selecionadas" | tr -d '"'
             break
         fi
     done
-
-    echo "$categorias" > categorias_selecionadas.txt
 }
